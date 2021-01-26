9f5672331ed9bda413ffb167347e7996f8482784
nisabalci <nisabalci00@gmail.com>
2021-01-27 02:06:49 +0300

    first commit

---
+++ MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void musteriEkle (Musteri musteri)
+        {
+            Console.WriteLine("Müşteri bilgileri eklendi.\n");
+        }
+        
+        public void musteriSil (Musteri musteri)
+        {
+            Console.WriteLine("Müşteri bilgileri silindi.\n");
+        }
+
+        public void musteriListele (Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + "\n" +
+                              musteri.Soyadi + "\n" + musteri.Yasi + "\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Nisa";
+            musteri1.Soyadi = "Balcı";
+            musteri1.Yasi = "25";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ayşe";
+            musteri2.Soyadi = "Güngör";
+            musteri2.Yasi = "32";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Ahmet";
+            musteri3.Soyadi = "Yılmaz";
+            musteri3.Yasi = "45";
+
+
+            Console.WriteLine("\n-------Müşteri Takip Sistemi-------\n");
+
+            //Ekleme
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.musteriEkle(musteri1);
+
+            //Silme
+            musteriManager.musteriSil(musteri2);
+
+            //Listeleme
+            Console.WriteLine("\nMüşteriler Listelendi :\n");
+            musteriManager.musteriListele(musteri1);
+            musteriManager.musteriListele(musteri2);
+            musteriManager.musteriListele(musteri3);
+
+
+
+        }
+    }
+}
